0feb96927544a1a49f115e9243716b69c0f45765
Dimas31321 <dyloboda@edu.hse.ru>
2026-06-09 20:40:21 +0000

    Выполнил первое задание

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" = "build_generator" ]; then
+    docker build -t data-generator -f DockerFile .
+elif [ "$1" = "run_generator" ]; then
+    mkdir -p data
+    docker run --rm -v "$(pwd)/data:/data" data-generator
+elif [ "$1" = "create_local_data" ]; then
+    mkdir -p local_data
+    python src/generate.py local_data
+else
+    echo "Usage:"
+    echo "./run.sh build_generator"
+    echo "./run.sh run_generator"
+    echo "./run.sh create_local_data"
+fi
--- src/generate.py
+++ src/generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["id", "country", "manufacturer", "item"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "id": random.randint(1, 1000),
+        "country": random.choice(["Russia", "USA", "Italy", "Germany"]),
+        "manufacturer": random.choice(["Company A", "Company B", "Company C", "Company D"]),
+        "item": random.choice(["Car", "T-shirt", "Keyboard"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
