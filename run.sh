5ba546edb336cbba05ad1ab17e0cbe4ef7bf40f7
Dimas31321 <dyloboda@edu.hse.ru>
2026-06-14 19:29:32 +0000

    Добавил улучшенную версию задания с сайтом

--- run.sh
+++ run.sh
@@ -12,23 +12,28 @@ elif [ "$1" = "build_reporter" ]; then
 elif [ "$1" = "run_reporter" ]; then
     docker run --rm -v "$(pwd)/data:/data" data-reporter
 elif [ "$1" = "structure" ]; then
-    docker run --rm data-reporter sh -c "find . -maxdepth 3 -print"
+    find . -maxdepth 3 -print
 elif [ "$1" = "clear_data" ]; then
     rm -rf data 
     rm -rf local_data
 elif [ "$1" = "inside_generator" ]; then
     docker run --rm -v "$(pwd)/data:/data" \
         data-generator \
-        sh -c "find /data -print"
+        find /data -print
 elif [ "$1" = "inside_reporter" ]; then 
     docker run --rm -v "$(pwd)/data:/data" \
         data-reporter \
-        sh -c "find /data -print"
+        find /data -print
 elif [ "$1" = "report_server" ]; then
-    docker run --rm \
+    docker run -d \
+        --name report-server \
         -p 8080:80 \
         -v "$(pwd)/data:/usr/share/nginx/html:ro" \
-        nginx
+        nginx 
+    echo "Чтобы открыть отчет, перейдите по ссылке: http://localhost:8080/report.html"
+    echo "Чтобы остановить сервер, выполните команду: ./run.sh stop_report_server"
+elif [ "$1" = "stop_report_server" ]; then
+    docker stop report-server
 else
     echo "Wrong command"
 fi
